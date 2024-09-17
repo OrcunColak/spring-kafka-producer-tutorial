d625c11cbe64276ca42f5a1629612b888388c282
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-17 09:01:03 +0300

    produce with transaction example

---
+++ src/main/java/com/colak/springtutorial/producer/TextProducer.java
@@ -0,0 +1,53 @@
+package com.colak.springtutorial.producer;
+
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+@RequiredArgsConstructor
+public class TextProducer {
+
+    // Constants for topic configuration
+    private static final int PARTITION_COUNT = 8;
+    private static final String TOPIC = "TEXT-DATA";
+    private static final short REPLICATION_FACTOR = 1;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @Autowired
+    public void configureTopic(KafkaAdmin kafkaAdmin) {
+        NewTopic newTopic = new NewTopic(TOPIC, PARTITION_COUNT, REPLICATION_FACTOR);
+        kafkaAdmin.createOrModifyTopics(newTopic);
+    }
+
+    public void sendMessages(List<String> messages) {
+        AtomicInteger counter = new AtomicInteger(0);
+        messages.forEach(line -> {
+            int lineIndex = counter.getAndIncrement();
+
+            // Sends the message to the topic, distributing across partitions based on the line index
+            String key = "KEY-" + (lineIndex % PARTITION_COUNT);
+            kafkaTemplate.send(TOPIC, key, line);
+        });
+    }
+
+    @Transactional
+    public void sendMessageWithTransaction(String message) {
+        kafkaTemplate.executeInTransaction(kafkaOperations -> {
+            kafkaOperations.send(TOPIC, message);
+
+            // Return any value to indicate transaction success
+            return true;
+        });
+    }
+
+}
---
+++ src/test/java/com/colak/springtutorial/producer/TextProducerTest.java
@@ -0,0 +1,26 @@
+package com.colak.springtutorial.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class TextProducerTest {
+
+    @Autowired
+    private TextProducer producer;
+
+    @Test
+    void testSendMessages() {
+        producer.sendMessages(List.of("line1", "line2"));
+    }
+
+    @Test
+    void testSendMessageWithTransaction() {
+        producer.sendMessageWithTransaction("test message");
+    }
+
+
+}
